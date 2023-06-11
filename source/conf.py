60b52c024fd16c37004ab8b1324b192d993144eb
wallace-lai <wallace-lai@foxmail.com>
2023-06-11 17:12:51 +0800

    add math formula support

--- source/conf.py
+++ source/conf.py
@@ -60,3 +60,17 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+import recommonmark
+from recommonmark.transform import AutoStructify
+
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+        #'url_resolver': lambda url: github_doc_root + url,
+        #'auto_toc_tree_section': 'Contents',
+        'enable_math': True,
+        'enable_inline_math': True,
+        #'enable_eval_rst': True,
+        #'enable_auto_doc_ref': True,
+    }, True)
+    app.add_transform(AutoStructify)
