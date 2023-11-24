7f0dfab832c482e52969306be868061d0d5d96ef
wallace-lai <wallace-lai@foxmail.com>
2023-11-24 23:40:08 +0800

    Fix css file

--- source/conf.py
+++ source/conf.py
@@ -65,6 +65,9 @@ html_theme = 'sphinx_rtd_theme'
 # Add custom css file
 html_css_files = ['css/custom.css']
 
+# Use custom styles in custom.css
+html_style = 'css/custom.css'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
