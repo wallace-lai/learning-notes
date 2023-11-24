534b413cfe31232e3e9b9aa91b02b979fd5026d7
wallace-lai <wallace-lai@foxmail.com>
2023-11-24 22:32:27 +0800

    remove 'Edit on' button'

--- source/conf.py
+++ source/conf.py
@@ -39,9 +39,6 @@ mathjax_path = 'MathJax/es5/tex-chtml.js'
 # Source suffix
 source_suffix = {'.rst' : 'restructuredtext', '.md' : 'markdown'}
 
-# Don't show source link
-html_show_sourcelink = False
-
 # Add any pa ths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
